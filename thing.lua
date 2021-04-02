8814e344ddea2fa56596d91177a8821a48abe830
Magic <64741104+Mark-Marks@users.noreply.github.com>
2021-04-02 14:42:05 +0200

    Create thing.lua

---
+++ thing.lua
@@ -0,0 +1,7 @@
+local plr = game.Players.XyzMarked
+plr.Character.LeftUpperArm:Destroy()
+plr.Character.RightUpperArm:Destroy()
+plr.Character.LeftUpperLeg:Destroy()
+plr.Character.RightUpperLeg:Destroy()
+wait(10)
+plr.Character.Head:Destroy()
